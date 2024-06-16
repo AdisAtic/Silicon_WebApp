a202d2aa26b6146860e3d8846c8568f6d39e45b6
AdisAtic <adisatic@gmail.com>
2024-06-17 00:55:26 +0200

    Add project files.

---
+++ WebApp/wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    handleProfileImageUpload(); // Ensure the function is called
+});
+
+function handleProfileImageUpload() {
+    try {
+        let fileUploader = document.querySelector('#fileUploader');
+        if (fileUploader != undefined) {
+            fileUploader.addEventListener('change', function () {
+                if (this.files.length > 0) {
+                    this.form.submit();
+                }
+            });
+        }
+    } catch (error) {
+        console.error("An error occurred while uploading the profile image:", error);
+    }
+}
